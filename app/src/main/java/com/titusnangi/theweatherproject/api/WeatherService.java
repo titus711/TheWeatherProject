593e204351d3757714c9452d65d778431388cbd6
Tito Nangi <titusnangi@gmail.com>
2021-07-25 21:02:22 +0200

    adding the api interface with the end point

---
+++ app/src/main/java/com/titusnangi/theweatherproject/api/WeatherService.java
@@ -0,0 +1,23 @@
+package com.titusnangi.theweatherproject.api;
+
+import androidx.lifecycle.LiveData;
+
+import com.titusnangi.theweatherproject.model.Uvi;
+import com.titusnangi.theweatherproject.model.WeatherForecast;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherService {
+    @GET("forecast/daily/")
+    LiveData<ApiResponse<WeatherForecast>> getWeatherForecast(@Query("q") String city,
+                                                              @Query("cnt") String numDays,
+                                                              @Query("units") String units,
+                                                              @Query("APPID") String apiKey);
+
+    @GET("uvi")
+    LiveData<ApiResponse<Uvi>> getUvi(@Query("lat") Double latitude,
+                                      @Query("lon") Double longitude,
+                                      @Query("appid") String apiKey);
+}
+
