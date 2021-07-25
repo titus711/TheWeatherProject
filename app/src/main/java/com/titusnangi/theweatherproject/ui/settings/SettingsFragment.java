540525d620d2e6baaf433cf01df13e8d90adba9f
Tito Nangi <titusnangi@gmail.com>
2021-07-25 18:02:50 +0200

    adding layout files and used the main activity bottom navigation to generate fragments

---
+++ app/src/main/java/com/titusnangi/theweatherproject/ui/settings/SettingsFragment.java
@@ -0,0 +1,20 @@
+package com.titusnangi.theweatherproject.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.titusnangi.theweatherproject.R;
+
+public class SettingsFragment extends Fragment {
+
+
+}
