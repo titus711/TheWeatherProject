5f41802f00c0974e3e2a43b1072e5669bb660ed5
Tito Nangi <titusnangi@gmail.com>
2021-07-25 22:13:32 +0200

    Update SettingsFragment.java

--- app/src/main/java/com/titusnangi/theweatherproject/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/titusnangi/theweatherproject/ui/settings/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.titusnangi.theweatherproject.ui.settings;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +14,61 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.titusnangi.theweatherproject.MainActivity;
 import com.titusnangi.theweatherproject.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment DialogFragment implements Injectable {
+
+
+    @Inject
+    NavigationController navigationController;
+
+    DataBindingComponent dataBindingComponent = new FragmentDataBindingComponent(this);
+    AutoClearedValue<SettingsFragmentBinding> binding;
+
+    public static SettingsFragment create() {
+        SettingsFragment settingsFragment = new SettingsFragment();
+        return settingsFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        SettingsFragmentBinding dataBinding = DataBindingUtil
+                .inflate(inflater, R.layout.settings_fragment, container, false,
+                        dataBindingComponent);
+        binding = new AutoClearedValue<>(this, dataBinding);
+        return dataBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        binding.get().setSettingsFragment(this);
+        binding.get().setCity(SharedPreferences.getInstance(getContext()).getCity());
+        binding.get().setNumDays(SharedPreferences.getInstance(getContext()).getNumDays());
+        binding.get().executePendingBindings();
+    }
+
+    public void didTapCancel(View view) {
+        dismiss();
+    }
+
+    public void didTapOk(View view) {
+
+        String newCity = binding.get().etSettingsCity.getText().toString();
+        String newNumDays = binding.get().etSettingsNumDays.getText().toString();
+
+        SharedPreferences.getInstance(getContext()).putStringValue(SharedPreferences.CITY, newCity);
+        SharedPreferences.getInstance(getContext()).putStringValue(SharedPreferences.NUM_DAYS, newNumDays);
+
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        startActivity(intent);
+
+        dismiss();
+    }
 
 
 }
